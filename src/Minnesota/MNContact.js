53d617eee356ede04d729146debd4d344e207d67
David Schwartz <dmschwartz@bis.midco.net>
2018-07-30 17:46:11 -0500

    added html for MNInput page

--- src/Minnesota/MNContact.js
+++ src/Minnesota/MNContact.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './CSS/MNContact.css';
 
 class MNContact extends React.Component {
 	render() {
 		return (
 			<div>
-
+			<Link to='/MN'>Back</Link>	
 			</div>
 		);
 	}
--- src/NorthDakota/NDContact.js
+++ src/NorthDakota/NDContact.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './CSS/NDContact.css';
 
 class NDContact extends React.Component {
 	render() {
 		return (
 			<div>
-
+				<Link to='/ND'>Back</Link>	
 			</div>
 		);
 	}
