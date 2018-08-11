4a87004d0c7523fca824d0331c5cfa8fab81f6bf
David Schwartz <dmschwartz@bis.midco.net>
2018-08-10 21:25:48 -0500

    fixed back button for NDOutput page, added github-pages again

--- src/NorthDakota/NDOutput.js
+++ src/NorthDakota/NDOutput.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 class NDOutput extends React.Component {
+	constructor(props) {
+		super(props)
+		this.props.resetCurrentAxle(); // must reset axle, or redirecting to MNInput will immediately redirect back
+	}
+	
 	render() {
 		return (
 			<div>
